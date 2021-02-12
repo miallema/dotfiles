b00868c13ed9b0ae5c4a86b69d1b37cf4ea9d91c
Michael Allemann <michael.allemann@protonmail.com>
2021-02-12 17:16:46 +0100

    removed git

--- my_environment.sh
+++ my_environment.sh
@@ -5,7 +5,6 @@ apt-get update && apt-get upgrade
 apt-get install vim
 apt-get install tmux
 apt-get install tldr
-apt-get install git
 apt-get install gnuradio
 sudo apt-get install python3-notebook jupyter jupyter-core 
 
